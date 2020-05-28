ea0ad3e3d1a31431c4091da12687b9d5d0602d41
Mojila <me.mojila@gmail.com>
2020-05-28 16:10:26 +0700

    load memory leak count

--- src/pages/analyze/_index.js
+++ src/pages/analyze/_index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Container, Card, CardContent, Grid, Typography, CardActions, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import MiniDrawer from '../../components/miniDrawer'
 import Context from '../../context'
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,8 +33,26 @@ const useStyles = makeStyles((theme) => ({
 export default function DetailAnalyze() {
   const { index } = useParams()
   const { store } = useContext(Context)
+  const [leaks, setLeaks] = useState([])
+  const history = useHistory()
   const classes = useStyles();
   const title = "Analyze"
+
+  const goBack = () => history.push('/analyze')
+
+  const loadLeaks = async () => {
+    let leakKey = `${store.pages[index]}-leak`
+    let data = await localStorage.getItem(leakKey)
+
+    if (data) {
+      setLeaks(JSON.parse(data))
+    }
+  }
+
+  useEffect(() => {
+    loadLeaks()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return <Container disableGutters fixed className={classes.root}>
     <MiniDrawer isOpen={true} title={title} />
@@ -43,7 +61,46 @@ export default function DetailAnalyze() {
       {/* content */}
       <Grid container spacing={2}>
         <Grid item md={3}>
-          Hello {index}
+          <Button variant="text" onClick={goBack}>Back</Button>
+        </Grid>
+        <Grid item md={9} />
+        <Grid item md={6}>
+          <Card>
+            <CardContent>
+              <Typography color="textSecondary">
+                Application Page
+              </Typography>
+              <Typography variant="subtitle1">
+                {store.pages[index]}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={6}>
+          <Card>
+            <CardContent>
+              <Typography color="textSecondary">
+                Memory Leak Detected
+              </Typography>
+              <Typography variant="subtitle1">
+                {leaks.length}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={3}>
+          <Card>
+            <CardContent>
+              list
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={9}>
+          <Card>
+            <CardContent>
+              visualize
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </main>
